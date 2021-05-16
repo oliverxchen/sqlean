4dd0ebf7fc6fcf300b9accb2aa2d551b9dcbbcd5
oliverxchen <oliver.chen@traveloka.com>
2021-05-16 11:29:34 +0800

    Add support for functions, split tests for readability (#3)

--- tests/test_sql_parser.py
+++ tests/test_sql_parser.py
@@ -1,32 +1,97 @@
-import pytest
 from sqlean.sql_parser import parser
 
 
-@pytest.fixture()
-def text():
-    return "select bar, baz as b, qux q from foo"
-
-
-def test_sql_parser(text):
-    actual = parser.parse(text)
-    print(actual)
+def test_simple_query():
+    raw_query = "select field from table"
+    actual = parser.parse(raw_query)
     assert actual.data == "query"
 
-    actual_query = actual.children
-    assert actual_query[0].type == "SELECT"
-    assert actual_query[1].data == "select_clause"
-    assert actual_query[2].type == "FROM"
-    assert actual_query[3].data == "from_item"
+    query = actual.children
+    assert query[0].type == "SELECT"
+    assert query[1].data == "select_list"
+    assert query[2].type == "FROM"
+    assert query[3].data == "from_item"
 
-    actual_select_items = actual_query[1].children
-    assert actual_select_items[0].type == "CNAME"
-    assert actual_select_items[0].value == "bar"
-    assert actual_select_items[1].children[0].type == "CNAME"
-    assert actual_select_items[1].children[0].value == "baz"
-    assert actual_select_items[1].children[1].type == "AS"
-    assert actual_select_items[1].children[2].type == "CNAME"
-    assert actual_select_items[1].children[2].value == "b"
-    assert actual_select_items[1].children[0].type == "CNAME"
-    assert actual_select_items[2].children[0].value == "qux"
-    assert actual_select_items[2].children[1].type == "CNAME"
-    assert actual_select_items[2].children[1].value == "q"
+
+def test_select_item_no_alias():
+    raw_query = "select field from table"
+    select_item = parser.parse(raw_query).children[1].children[0]
+
+    assert select_item.data == "select_item"
+    assert select_item.children[0].type == "CNAME"
+    assert select_item.children[0].value == "field"
+
+
+def test_select_item_implicit_alias():
+    raw_query = "select field new_field_name from table"
+    select_item = parser.parse(raw_query).children[1].children[0]
+
+    assert select_item.data == "select_item"
+    assert select_item.children[0].type == "CNAME"
+    assert select_item.children[0].value == "field"
+    assert select_item.children[1].data == "alias"
+    assert select_item.children[1].children[0].value == "new_field_name"
+
+
+def test_select_item_explicit_alias():
+    raw_query = "select field as new_field_name from table"
+    select_item = parser.parse(raw_query).children[1].children[0]
+
+    assert select_item.data == "select_item"
+    assert select_item.children[0].type == "CNAME"
+    assert select_item.children[0].value == "field"
+    assert select_item.children[1].data == "alias"
+    assert select_item.children[1].children[0].type == "AS"
+    assert select_item.children[1].children[1].value == "new_field_name"
+
+
+def test_arg_item_generic():
+    raw_query = "select SUM(x) from c"
+    select_item = parser.parse(raw_query).children[1].children[0]
+
+    assert select_item.data == "select_item"
+    assert select_item.children[0].data == "select_value"
+    assert select_item.children[0].children[0].type == "FUNCTION"
+    assert select_item.children[0].children[0].value == "SUM"
+    assert select_item.children[0].children[1].type == "LPAR"
+    assert select_item.children[0].children[3].type == "RPAR"
+
+    arg_list_one = select_item.children[0].children[2]
+    assert arg_list_one.data == "arg_list"
+    assert arg_list_one.children[0].value == "x"
+
+
+def test_arg_item_with_date_interval():
+    raw_query = "select date_trunc(a, month) from c"
+    select_item = parser.parse(raw_query).children[1].children[0]
+
+    assert select_item.data == "select_item"
+    assert select_item.children[0].data == "select_value"
+    assert select_item.children[0].children[0].type == "FUNCTION"
+    assert select_item.children[0].children[0].value == "date_trunc"
+    assert select_item.children[0].children[1].type == "LPAR"
+    assert select_item.children[0].children[3].type == "RPAR"
+
+    arg_list_two = select_item.children[0].children[2]
+    assert arg_list_two.data == "arg_list"
+    assert arg_list_two.children[0].value == "a"
+    assert arg_list_two.children[1].type == "DATE_INTERVAL"
+    assert arg_list_two.children[1].value == "month"
+
+
+def test_arg_item_with_time_interval():
+    raw_query = "select time_trunc(b, SECOND) from c"
+    select_item = parser.parse(raw_query).children[1].children[0]
+
+    assert select_item.data == "select_item"
+    assert select_item.children[0].data == "select_value"
+    assert select_item.children[0].children[0].type == "FUNCTION"
+    assert select_item.children[0].children[0].value == "time_trunc"
+    assert select_item.children[0].children[1].type == "LPAR"
+    assert select_item.children[0].children[3].type == "RPAR"
+
+    arg_list_three = select_item.children[0].children[2]
+    assert arg_list_three.data == "arg_list"
+    assert arg_list_three.children[0].value == "b"
+    assert arg_list_three.children[1].type == "TIME_INTERVAL"
+    assert arg_list_three.children[1].value == "SECOND"
