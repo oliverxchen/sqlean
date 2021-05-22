929978e3a36277b8b2ec50e2014d224f509699f7
Oliver Chen <oliver.chen@traveloka.com>
2021-05-22 14:11:12 +0800

    move .lark file into separate  sub directory and remove hard coded path

--- sqlean/sql_parser.py
+++ sqlean/sql_parser.py
@@ -1,7 +1,7 @@
 """Parse sql strings"""
-
+import os.path as path
 from lark import Lark
 
 
-IMPORT_PATH = "/Users/oliver.chen/src/sqlean/sqlean"
-parser = Lark.open("sql.lark", rel_to=__file__, import_paths=[IMPORT_PATH])
+IMPORT_PATH = path.join(path.dirname(__file__), "grammar")
+parser = Lark.open("grammar/sql.lark", rel_to=__file__, import_paths=[IMPORT_PATH])
