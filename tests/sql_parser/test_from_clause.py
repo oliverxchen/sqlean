2735bc345249f337fa5bc33e572a7ccaa8bd55c8
Oliver Chen <oliver.chen@traveloka.com>
2021-05-30 19:19:42 +0800

    From clause
    * recombine from.lark into main sql.lark to avoid circular dependency
    * allow sub-queries

--- tests/sql_parser/test_from_clause.py
+++ tests/sql_parser/test_from_clause.py
@@ -1,7 +1,7 @@
 def test_single_table(parser):
     raw_query = "select field from table"
-    from_clause = parser.get_tree(raw_query).children[3]
 
+    from_clause = parser.get_tree(raw_query).children[3]
     assert from_clause.data == "from_clause"
     assert from_clause.children[0].data == "table_id"
     assert from_clause.children[0].children[0].type == "CNAME"
@@ -10,8 +10,8 @@ def test_single_table(parser):
 
 def test_single_table_outerbackticks(parser):
     raw_query = "select field from `project.dataset.table`"
-    from_clause = parser.get_tree(raw_query).children[3]
 
+    from_clause = parser.get_tree(raw_query).children[3]
     assert from_clause.data == "from_clause"
     assert from_clause.children[0].data == "table_id"
     assert from_clause.children[0].children[0].type == "PROJECT_NAME"
@@ -24,8 +24,8 @@ def test_single_table_outerbackticks(parser):
 
 def test_single_table_multiplebackticks(parser):
     raw_query = "select field from `project`.`dataset`.`table`"
-    from_clause = parser.get_tree(raw_query).children[3]
 
+    from_clause = parser.get_tree(raw_query).children[3]
     assert from_clause.data == "from_clause"
     assert from_clause.children[0].data == "table_id"
     assert from_clause.children[0].children[0].type == "PROJECT_NAME"
@@ -34,3 +34,18 @@ def test_single_table_multiplebackticks(parser):
     assert from_clause.children[0].children[1].value == "dataset"
     assert from_clause.children[0].children[2].type == "TABLE_NAME"
     assert from_clause.children[0].children[2].value == "table"
+
+
+def test_sub_query(parser):
+    raw_query = "select field from (select field, field2 from table)"
+
+    from_clause = parser.get_tree(raw_query).children[3]
+    assert from_clause.data == "from_clause"
+    assert from_clause.children[0].data == "table_id"
+
+    sub_query = from_clause.children[0].children[0]
+    assert sub_query.data == "query"
+    assert sub_query.children[0].type == "SELECT"
+    assert sub_query.children[1].data == "select_list"
+    assert sub_query.children[2].type == "FROM"
+    assert sub_query.children[3].data == "from_clause"
