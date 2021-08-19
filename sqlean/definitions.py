5af99836d1f3d4ed56dfbfe108a39d478f186c30
Oliver Chen <oliver.chen@traveloka.com>
2021-08-20 05:40:50 +0800

    Style snapshotting
    * Snapshot testing implemented for styles
    * Change field_name to token

--- sqlean/definitions.py
+++ sqlean/definitions.py
@@ -3,4 +3,5 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent.parent
 TREE_SNAPSHOT_PATH = ROOT_PATH / "tests" / "tree_snapshots"
+STYLE_SNAPSHOT_PATH = ROOT_PATH / "tests" / "style_snapshots"
 IMPORT_PATH = ROOT_PATH / "sqlean" / "grammar"
